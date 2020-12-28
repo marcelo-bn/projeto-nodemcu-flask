ec9a532885f03dfa69bf70a1f39873e045a0fedc
marcelo-bn <marcelobittencourtx@gmail.com>
2020-12-28 15:58:27 -0300

    /bomba GET

--- pycharm-sql/app.py
+++ pycharm-sql/app.py
@@ -77,7 +77,6 @@ def cadastra_vegetal():
 # App mobile realiza para alterar vegetal cadastrado
 @app.route('/vegetal', methods=['PUT'])
 def altera_vegetal():
-
     # Conexão com o banco
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -100,7 +99,6 @@ def altera_vegetal():
 # App mobile realiza para obter o estado dos vasos
 @app.route('/vaso', methods=['GET'])
 def obtem_vaso():
-
     # Conexão com o banco
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -115,7 +113,7 @@ def obtem_vaso():
     try:
         for item in info:
             lista_vasos.append({"id": item[0], "status": item[1], "bomba": item[2],
-            "tempo": item[3], "vegetal": item[4]})
+                                "tempo": item[3], "vegetal": item[4]})
 
         return jsonify({'lista_vasos': lista_vasos})
     except:
@@ -125,7 +123,6 @@ def obtem_vaso():
 # App mobile realiza para alterar o estado dos vasos (Informa o vegetal)
 @app.route('/vaso', methods=['PUT'])
 def altera_vaso():
-
     # Conexão com o banco
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -146,7 +143,6 @@ def altera_vaso():
 # App mobile realiza para desligar os vasos
 @app.route('/vaso', methods=['DELETE'])
 def desliga_vaso():
-
     # Conexão com o banco
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -165,7 +161,6 @@ def desliga_vaso():
 # App mobile realiza para ligar a bomba dos vasos
 @app.route('/bomba', methods=['PUT'])
 def ativa_bomba():
-
     # Conexão com o banco
     banco = sqlite3.connect('banco.db')
     cursor = banco.cursor()
@@ -205,22 +200,64 @@ def obtem_info():
     return jsonify({'lista_info': lista_info})
 
 
-
-
-
-
-
-
-
 # Nodemcu realiza para verificar se deve ligar a bomba
 @app.route('/bomba', methods=['GET'])
-def obtem_bomba():
-    global lista_bomba
-    lista_bomba_aux = lista_bomba
-    lista_bomba = []
-    return jsonify({'lista_bomba': lista_bomba_aux})
+def liga_bomba():
+    # Conexão com o banco
+    banco = sqlite3.connect('banco.db')
+    cursor = banco.cursor()
+
+    # Lista de vasos que devem ligar a bomba
+    lista_vasos_bomba = []
+
+    # Selecionando os dados do banco
+    query_str = 'SELECT * FROM Vaso'
+    info = cursor.execute(query_str).fetchall()
+
+    for item in info:
+        if item[2] == 1:
+            lista_vasos_bomba.append({"idVaso": item[0], "tempo": item[3]})
+            query_str = 'UPDATE Vaso SET tempo = 0, bomba = 0 WHERE id = ' + str(item[0]) # Zera novamente a bomba do vaso
+            cursor.execute(query_str)
+            banco.commit()
+
+    return jsonify({'lista_vasos_bomba': lista_vasos_bomba})
+
+'''
+# Nodemcu realiza para inserir informação no banco
+@app.route('/informacao', methods=['POST'])
+def add_info():
+
+    # Conexão com o banco
+    banco = sqlite3.connect('banco.db')
+    cursor = banco.cursor()
+
+    # Leitura dos parâmetros recebidos
+    idVaso = request.json.get('idVaso')
+    temperatura = request.json.get('t')
+    umidade = request.json.get('u')
+    data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 
+
+
+    if conjunto_vegetal[int(idConjunto) - 1] != '':
+        # Inserção no banco
+        query_str = 'INSERT INTO Sistema (idConjunto,temperatura,umidade,tipo,data) VALUES (\'' \
+                    + idConjunto + '\',\'' + temperatura + '\',\'' + umidade + '\',\'' + conjunto_vegetal[
+                        int(idConjunto) - 1] + '\',\'' + data + '\')'
+        cursor.execute(query_str)
+
+        # Analisando situação do vegetal        verifica_medidas(idConjunto, temperatura, umidade, conjunto_vegetal[int(idConjunto)-1])
+
+        try:
+            banco.commit()
+            return make_response(jsonify('Objeto cadastrado!'), 200)
+        except Exception as e:
+            return make_response(jsonify('Objeto não cadastrado!'), 406)
+'''
+
+'''
 # Nodemcu realiza para inserir informação no banco
 @app.route('/informacao', methods=['POST'])
 def add_info():
@@ -239,7 +276,8 @@ def add_info():
     if conjunto_vegetal[int(idConjunto) - 1] != '':
         # Inserção no banco
         query_str = 'INSERT INTO Sistema (idConjunto,temperatura,umidade,tipo,data) VALUES (\'' \
-                    + idConjunto + '\',\'' + temperatura + '\',\'' + umidade + '\',\'' + conjunto_vegetal[int(idConjunto)-1] + '\',\'' + data + '\')'
+                    + idConjunto + '\',\'' + temperatura + '\',\'' + umidade + '\',\'' + conjunto_vegetal[
+                        int(idConjunto) - 1] + '\',\'' + data + '\')'
         cursor.execute(query_str)
 
         # Analisando situação do vegetal        verifica_medidas(idConjunto, temperatura, umidade, conjunto_vegetal[int(idConjunto)-1])
@@ -267,7 +305,7 @@ def ativos():
         return make_response(jsonify('Ativação concluída!'), 200)
     except Exception as e:
         return make_response(jsonify('Erro ao realizar operação!'), 406)
-
+'''
 
 # Verifica se precisa acionar a bomba e adiciona na lista de bomba
 def verifica_medidas(idConjunto, temperatura, umidade, tipo):
